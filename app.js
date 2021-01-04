e98575e982ec4aae738979163fc6d08b62e70e1c
Otg <otgonjargal.natsag@gmail.com>
2021-01-05 07:46:46 +0800

    Income and expenses will be displayed at UI.

--- app.js
+++ app.js
@@ -1,56 +1,143 @@
-/*
-//Дэлгэцтэй ажиллах контроллер
-var uiController = (function () {})();
-
-//Санхүүтэй ажиллах контроллер
-var financeController = (function () {})();
-
-//Программын холбогч контроллер
-var appController = (function (uiController, financeController) {
-  document.querySelector(".add__btn").addEventListener("click", function () {
-    //console.log("clicked..."); //nuguu sumiig click hiiheer 'clicked' gj garch bna
-    // 1.Оруулах өгөгдлийг дэлгэцээс олж авна.
-    // 2.Олж авсан өгөгдлүүдээ Санхүүгийн контроллерт дамжуулж түүнд хадгална.
-    // 3.Олж авсан өгөгдлүүдийг вэб дээрээ тохирох хэсэгт нь гаргана.
-    // 4.Төслийг тооцоолно.
-    // 5. Эцсийн үлдэгдэл тооцоог дэлгэцэнд гаргана.
-  });
-  //Заавал хулганаар дарж орлого, зарлагыг оруулахаас гадна ENTER дарж болно. Тиймээс Google дээр Javascript event mdn гэж хайх
-  //Keyboard-ийн нэг ялгаа нь аль нэг сэгмент дээр биш нийт пэйж дээрээ код нь бичигддэг онцлогтой.
-  document.addEventListener("keypress", function (event) {
-    //console.log("Товч дарагдлаа");
-    //console.log(event);
-    //if (event.keyCode === 13) console.log("Enter дарсан байна.");
-    //else console.log("Өөр товч дарсан байна: " + event.keyCode);
-    if (event.keyCode === 13) console.log("Дэлгэцээс өгөгдлөө авах хэсэг.");
-  });
-})(uiController, financeController);
-
-*/
-
-//Дэлгэцтэй ажиллах контроллер
-var uiController = (function () {})();
-
-//Санхүүтэй ажиллах контроллер
-var financeController = (function () {})();
-
-//Программын холбогч контроллер
-var appController = (function (uiController, financeController) {
-  //Саяны key event үйлдэлүүлийг хийдэг 1 функцийг энд бичье!!
-  var ctrlAddItem = function () {
-    // 1.Оруулах өгөгдлийг дэлгэцээс олж авна.
-    console.log("Дэлгэцээс өгөгдлөө авах хэсэг");
-    // 2.Олж авсан өгөгдлүүдээ Санхүүгийн контроллерт дамжуулж түүнд хадгална.
-    // 3.Олж авсан өгөгдлүүдийг вэб дээрээ тохирох хэсэгт нь гаргана.
-    // 4.Төслийг тооцоолно.
-    // 5. Эцсийн үлдэгдэл тооцоог дэлгэцэнд гаргана.
+// Дэлгэцтэй ажиллах контроллер
+var uiController = (function () {
+  var DOMstrings = {
+    inputType: ".add__type",
+    inputDescription: ".add__description",
+    inputValue: ".add__value",
+    addBtn: ".add__btn",
   };
-  document.querySelector(".add__btn").addEventListener("click", function () {
-    ctrlAddItem();
-  });
-  document.addEventListener("keypress", function (event) {
-    if (event.keyCode === 13 || event.which === 13) {
+
+  return {
+    getInput: function () {
+      return {
+        type: document.querySelector(DOMstrings.inputType).value, // exp, inc
+        description: document.querySelector(DOMstrings.inputDescription).value,
+        value: document.querySelector(DOMstrings.inputValue).value,
+      };
+    },
+    //Public service
+    getDOMstrings: function () {
+      return DOMstrings;
+    },
+
+    //Public service
+    addListItem: function (item, type) {
+      // 1. Орлого, зарлагын элэментийг агуулсан html-ийг бэлтгэнэ.
+      var html, list;
+      if (type === "inc") {
+        list = ".income__list";
+        html =
+          '<div class="item clearfix" id="income-%id%"><div class="item__description">$$DESCRIPTION$$</div><div class="right clearfix"><div class="item__value">$$VALUE$$</div><div class="item__delete"><button class="item__delete--btn"><i class="ion-ios-close-outline"></i></button></div>    </div></div>';
+      } else {
+        list = ".expenses__list";
+        html =
+          '<div class="item clearfix" id="expense-%id%"><div class="item__description">$$DESCRIPTION$$</div><div class="right clearfix"><div class="item__value">$$VALUE$$</div><div class="item__percentage">21%</div><div class="item__delete"><button class="item__delete--btn"><i class="ion-ios-close-outline"></i></button></div></div></div>';
+      }
+      // 2. Тэр нь HTML дотроо орлого зарлагын утгуудыг REPLACE ашиглан өөрчилж өгнө.
+      html = html.replace("%id%", item.id);
+      html = html.replace("$$DESCRIPTION$$", item.description);
+      html = html.replace("$$VALUE$$", item.value);
+
+      // 3. Бэлтгэсэн HTML-ээ DOM-руу хийж өгнө.
+      document.querySelector(list).insertAdjacentHTML("beforeend", html);
+    },
+  };
+})();
+
+// Санхүүтэй ажиллах контроллер
+var financeController = (function () {
+  // private data
+  var Income = function (id, description, value) {
+    this.id = id;
+    this.description = description;
+    this.value = value;
+  };
+
+  // private data
+  var Expense = function (id, description, value) {
+    this.id = id;
+    this.description = description;
+    this.value = value;
+  };
+
+  // private data
+  var data = {
+    items: {
+      inc: [],
+      exp: [],
+    },
+
+    totals: {
+      inc: 0,
+      exp: 0,
+    },
+  };
+
+  return {
+    addItem: function (type, desc, val) {
+      var item, id;
+
+      if (data.items[type].length === 0) id = 1;
+      else {
+        id = data.items[type][data.items[type].length - 1].id + 1;
+      }
+
+      if (type === "inc") {
+        item = new Income(id, desc, val);
+      } else {
+        item = new Expense(id, desc, val);
+      }
+
+      data.items[type].push(item);
+
+      return item;
+    },
+
+    seeData: function () {
+      return data;
+    },
+  };
+})();
+
+// Програмын холбогч контроллер
+var appController = (function (uiController, financeController) {
+  var ctrlAddItem = function () {
+    // 1. Оруулах өгөгдлийг дэлгэцээс олж авна.
+    var input = uiController.getInput();
+
+    // 2. Олж авсан өгөгдлүүдээ санхүүгийн контроллерт дамжуулж тэнд хадгална.
+    var item = financeController.addItem(
+      input.type,
+      input.description,
+      input.value
+    );
+
+    // 3. Олж авсан өгөгдлүүдээ вэб дээрээ тохирох хэсэгт нь гаргана
+    uiController.addListItem(item, input.type);
+    // 4. Төсвийг тооцоолно
+    // 5. Эцсийн үлдэгдэл, тооцоог дэлгэцэнд гаргана.
+  };
+
+  var setupEventListeners = function () {
+    var DOM = uiController.getDOMstrings();
+
+    document.querySelector(DOM.addBtn).addEventListener("click", function () {
       ctrlAddItem();
-    }
-  });
+    });
+
+    document.addEventListener("keypress", function (event) {
+      if (event.keyCode === 13 || event.which === 13) {
+        ctrlAddItem();
+      }
+    });
+  };
+
+  return {
+    init: function () {
+      console.log("Application started...");
+      setupEventListeners();
+    },
+  };
 })(uiController, financeController);
+
+appController.init();
