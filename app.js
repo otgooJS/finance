f7511555b5810da63c5cc43bb7c8f121cc0a1291
Otg <otgonjargal.natsag@gmail.com>
2021-01-05 09:41:03 +0800

    Orlogo, zarlagaas tusub bodogddog bolno

--- app.js
+++ app.js
@@ -5,6 +5,8 @@ var uiController = (function () {
     inputDescription: ".add__description",
     inputValue: ".add__value",
     addBtn: ".add__btn",
+    incomeList: ".income__list",
+    expenseList: ".expenses__list",
   };
 
   return {
@@ -12,7 +14,7 @@ var uiController = (function () {
       return {
         type: document.querySelector(DOMstrings.inputType).value, // exp, inc
         description: document.querySelector(DOMstrings.inputDescription).value,
-        value: document.querySelector(DOMstrings.inputValue).value,
+        value: parseInt(document.querySelector(DOMstrings.inputValue).value), //parseInt: convert string to number
       };
     },
     //Public service
@@ -20,16 +22,40 @@ var uiController = (function () {
       return DOMstrings;
     },
 
+    clearFields: function () {
+      // fields is not massive, is list
+      var fields = document.querySelectorAll(
+        DOMstrings.inputDescription + "," + DOMstrings.inputValue
+      );
+      //let's convert fields/list to massive: bagsh: convert list to Array
+      //var newArr=arr.slice.call(list)
+      var fieldsArr = Array.prototype.slice.call(fields);
+
+      // Circle-iig for each ashiglan arai short-oor hiij bolno
+      //for (var i = 0; i < fieldsArr.length; i++) {
+      //  fieldsArr[i].value = "";
+      //}
+
+      //fieldsArr.ForEach(); //element bolgoniih ni hyvid
+      //ForEach-s Ano f ugnu. 1.el=davaj bgaa element 2. index=Ter element ni yamar indextei ve
+      // 3. array= uuriig ni bytsaah
+      fieldsArr.forEach(function (el, index, array) {
+        el.value = "";
+      });
+
+      fieldsArr[0].focus();
+    },
+
     //Public service
     addListItem: function (item, type) {
       // 1. Орлого, зарлагын элэментийг агуулсан html-ийг бэлтгэнэ.
       var html, list;
       if (type === "inc") {
-        list = ".income__list";
+        list = DOMstrings.incomeList;
         html =
           '<div class="item clearfix" id="income-%id%"><div class="item__description">$$DESCRIPTION$$</div><div class="right clearfix"><div class="item__value">$$VALUE$$</div><div class="item__delete"><button class="item__delete--btn"><i class="ion-ios-close-outline"></i></button></div>    </div></div>';
       } else {
-        list = ".expenses__list";
+        list = DOMstrings.expenseList;
         html =
           '<div class="item clearfix" id="expense-%id%"><div class="item__description">$$DESCRIPTION$$</div><div class="right clearfix"><div class="item__value">$$VALUE$$</div><div class="item__percentage">21%</div><div class="item__delete"><button class="item__delete--btn"><i class="ion-ios-close-outline"></i></button></div></div></div>';
       }
@@ -59,6 +85,14 @@ var financeController = (function () {
     this.description = description;
     this.value = value;
   };
+  var calculateTotal = function (type) {
+    var sum = 0;
+    data.items[type].forEach(function (el) {
+      sum = sum + el.value;
+    });
+
+    data.totals[type] = sum;
+  };
 
   // private data
   var data = {
@@ -71,9 +105,30 @@ var financeController = (function () {
       inc: 0,
       exp: 0,
     },
+
+    tusuv: 0,
+
+    huvi: 0,
   };
 
   return {
+    tusuvTootsooloh: function () {
+      //sanhuugiin dald f
+      calculateTotal("inc"); //Нийт орлогын нийлбэрийг тооцоолно
+      calculateTotal("exp"); //Нийт зарлагын нийлбэрийг тооцоолно
+      data.tusuv = data.totals.inc - data.totals.exp; //Төсвийг шинээр тооцоолно
+      //Орлго, зарлагын хувийг тооцоолно
+      data.huvi = Math.round((data.totals.exp / data.totals.inc) * 100);
+    },
+    tusviigAvah: function () {
+      return {
+        tusuv: data.tusuv,
+        huvi: data.huvi,
+        totalInc: data.totals.inc,
+        totalExp: data.totals.exp,
+      };
+    },
+
     addItem: function (type, desc, val) {
       var item, id;
 
@@ -105,17 +160,24 @@ var appController = (function (uiController, financeController) {
     // 1. Оруулах өгөгдлийг дэлгэцээс олж авна.
     var input = uiController.getInput();
 
-    // 2. Олж авсан өгөгдлүүдээ санхүүгийн контроллерт дамжуулж тэнд хадгална.
-    var item = financeController.addItem(
-      input.type,
-      input.description,
-      input.value
-    );
-
-    // 3. Олж авсан өгөгдлүүдээ вэб дээрээ тохирох хэсэгт нь гаргана
-    uiController.addListItem(item, input.type);
-    // 4. Төсвийг тооцоолно
-    // 5. Эцсийн үлдэгдэл, тооцоог дэлгэцэнд гаргана.
+    if (input.description !== "" && input.value !== "") {
+      // 2. Олж авсан өгөгдлүүдээ санхүүгийн контроллерт дамжуулж тэнд хадгална.
+      var item = financeController.addItem(
+        input.type,
+        input.description,
+        input.value
+      );
+      // 3. Олж авсан өгөгдлүүдээ вэб дээрээ тохирох хэсэгт нь гаргана
+      uiController.addListItem(item, input.type);
+      uiController.clearFields();
+      // 4. Төсвийг тооцоолно
+      //code-iig hoinoos ni bichij boldog. Bidend tusuvTsootsooloh gesen f Not yet
+      financeController.tusuvTootsooloh();
+      // 5. Эцсийн үлдэгдэл, тооцоог дэлгэцэнд гаргана.
+      var tusuv = financeController.tusviigAvah();
+      // 6. Төсвийн тооцоог дэлгэцэнд гаргана.
+      console.log(tusuv);
+    }
   };
 
   var setupEventListeners = function () {
